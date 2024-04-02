0a61f9626032c1b59f2c1c67dc37e88312396bde
Rodrigo <201910581@urp.edu.pe>
2024-04-01 21:36:42 -0500

    dashboards v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardGerenteComponent } from './components/dashboard-gerente/dashboard-gerente.component';
+import { DashboardTrabajadorComponent } from './components/dashboard-trabajador/dashboard-trabajador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'dashboard-gerente', component: DashboardGerenteComponent},
+  { path: 'dashboard-trabajador', component: DashboardTrabajadorComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
