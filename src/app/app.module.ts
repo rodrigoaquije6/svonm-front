ed98ac872354c8f63f28d4e9a5817ce87a501fc0
Rodrigo <201910581@urp.edu.pe>
2024-04-07 19:43:04 -0500

    front ejm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +32,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/roles/crear-rol/crear-rol.component.ts
+++ src/app/components/roles/crear-rol/crear-rol.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Rol } from 'src/app/models/rol';
 
 @Component({
@@ -12,9 +13,10 @@ export class CrearRolComponent implements OnInit{
   rolForm: FormGroup;
 
   constructor(private fb: FormBuilder,
+              private toastr: ToastrService,
               private router: Router) {
     this.rolForm = this.fb.group({
-      nombre: ['', Validators.required]
+      nombre: ['', Validators.required],
     })
    }
 
@@ -22,14 +24,14 @@ export class CrearRolComponent implements OnInit{
   }
 
   agregarRol() {
-    console.log(this,this.rolForm)
 
     const ROL: Rol = {
       nombre: this.rolForm.get('nombre')?.value
     }
 
-    console.log(ROL)
-    this.router.navigate(['/dashboard-gerente/rol'])
+    console.log(ROL);
+    this.toastr.success('El rol fue registrado con éxito!', 'Rol Registrado!');
+    this.router.navigate(['/dashboard-gerente/rol']);
   }
 
 }
