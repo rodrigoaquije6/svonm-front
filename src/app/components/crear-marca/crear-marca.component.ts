64eb8bf2d1e82a1bd5574dadb804eb1e5629cb47
FabrizioPanduro <fabriziopanduro@gmail.com>
2024-04-10 14:29:27 -0500

    Cambio 3 Front crear marca - Panduro

--- src/app/components/crear-marca/crear-marca.component.ts
+++ src/app/components/crear-marca/crear-marca.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { marca } from 'src/app/models/marca';
 
 @Component({
-  selector: 'app-marca',
+  selector: 'app-crear-marca',
   templateUrl: './crear-marca.component.html',
   styleUrls: ['./crear-marca.component.css']
 })
-export class CrearMarcaComponent {
+export class CrearMarcaComponent implements OnInit{
+
+  crearmarcaForm: FormGroup;
+
+
+  constructor(private fb:FormBuilder, private router: Router, private toastr: ToastrService){
+    this.crearmarcaForm = this.fb.group({
+      codigo:['',Validators.required],
+      nombre:['',Validators.required]
+    })
+  }
+
+ngOnInit(): void {
+}
+
+agregarMarca(){
+
+
+  const CODIGO: marca = {
+    codigo:this.crearmarcaForm.get('codigo')?.value,
+    nombre: this.crearmarcaForm.get('nombre')?.value
+
+  }
+  
+  console.log(CODIGO);
+  this.toastr.success('La marca fue registrada con éxito!', 'Marca registrada!')
+  this.router.navigate(['/dashboard-gerente/marca'])
+
+}
 
 }
